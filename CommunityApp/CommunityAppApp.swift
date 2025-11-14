c8a7fc23c486badf1203ec9317799059b559d205
Jayden Seto <setoja@sheridancollege.ca>
2025-11-14 17:23:18 -0500

    Initial Commit

---
+++ CommunityApp/CommunityAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CommunityAppApp.swift
+//  CommunityApp
+//
+//  Created by Jayden Seto on 2025-11-14.
+//
+
+import SwiftUI
+
+@main
+struct CommunityAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ CommunityAppTests/CommunityAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityAppTests.swift
+//  CommunityAppTests
+//
+//  Created by Jayden Seto on 2025-11-14.
+//
+
+import Testing
+@testable import CommunityApp
+
+struct CommunityAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
